a39c538e42e5f86f49cf1b45c82e8ed1c64bd79b
M Haidar Hanif <git@mhaidarhanif.com>
2023-03-16 01:32:53 +0700

    feat: import assets from previous website

---
+++ app/_old-routes/artworks.$artworkSlug.tsx
@@ -0,0 +1,53 @@
+// @ts-nocheck
+import { gql } from "@urql/core";
+import { FunctionComponent } from "react";
+import { json, LoaderFunction, useLoaderData, useParams } from "remix";
+import { Artwork } from "~/components";
+import { graphcmsClient } from "~/lib";
+
+interface ArtworkSlugProps {}
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const { artworkSlug } = params;
+
+  const queryArtworkBySlug = gql`
+    query ArtworkBySlug($slug: String!) {
+      artwork(where: { slug: $slug }) {
+        id
+        title
+        slug
+        images {
+          url(transformation: { image: { resize: { width: 500 } } })
+        }
+        medium
+        size
+        date
+        price
+        artists {
+          id
+          name
+          username
+        }
+      }
+    }
+  `;
+
+  const response = await graphcmsClient
+    .query(queryArtworkBySlug, { slug: artworkSlug })
+    .toPromise();
+  const { artwork } = response.data;
+
+  return json(artwork);
+};
+
+const ArtworkSlug: FunctionComponent<ArtworkSlugProps> = () => {
+  const artwork = useLoaderData();
+
+  return (
+    <div>
+      <Artwork artwork={artwork} />
+    </div>
+  );
+};
+
+export default ArtworkSlug;
--- app/components/shared/landing-image.tsx
+++ app/components/shared/landing-image.tsx
@@ -1,17 +1,7 @@
-import { Theme, useTheme } from "remix-themes";
-
 export function LandingImage() {
-  const [theme] = useTheme();
-  const themeName = theme === Theme.DARK ? "dark" : "light";
-
   return (
     <div>
-      <img
-        src={`/assets/images/cat-study-${themeName}.png`}
-        alt="Cat Study"
-        width={300}
-        height={300}
-      />
+      <span>Image here</span>
     </div>
   );
 }
