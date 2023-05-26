e71233c50ede391aad32b8195ef7807279eb8796
M Haidar Hanif <git@mhaidarhanif.com>
2023-05-26 09:23:15 +0700

    chore: upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,7 +62,6 @@ module.exports = {
   },
   plugins: [
     require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
     require("@tailwindcss/typography"),
     require("tailwindcss-animate"),
     require("tailwindcss-debug-screens"),
