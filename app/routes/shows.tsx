f93f32e637c1ad9e8fa2fa5ace9859b8f6ef5909
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2023-06-14 23:50:44 -0400

    Show exhibitions data

--- app/routes/shows.tsx
+++ app/routes/shows.tsx
@@ -1,30 +1,43 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction} from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 import { Layout, PageHeader } from "~/components";
+import { prisma } from "~/libs";
 import { createDocumentLinks, createMetaData, createSitemap } from "~/utils";
 
 export const meta = createMetaData({
-  title: "Shows",
+  title: "Exhibitions",
   description: "Some description.",
 });
 
-export const handle = createSitemap("/shows", 0.9);
+export const handle = createSitemap("/exhibitions", 0.9);
 
 export const links: LinksFunction = () => {
-  return createDocumentLinks({ canonicalPath: "/shows" });
+  return createDocumentLinks({ canonicalPath: "/exhibitions" });
 };
 
+export async function loader() {
+  const exhibitions = await prisma.exhibition.findMany({
+    include: {
+      images: true,
+    },
+  });
+  return json({ exhibitions });
+}
+
 export default function ShowsRoute() {
+  const { exhibitions } = useLoaderData<typeof loader>();
   return (
     <Layout
       isSpaced
       pageHeader={
         <PageHeader size="sm" isTextCentered>
-          <h2>Shows</h2>
+          <h2>Exhibitions</h2>
         </PageHeader>
       }
     >
-      <p>Change something here.</p>
+      <p>{JSON.stringify(exhibitions)}</p>
     </Layout>
   );
 }
